04ebefa66d40951df14733a4392d761032367777
winniekwoknorth <winniekwokuk@yahoo.com>
2024-08-02 17:55:18 +0100

    change viteconfig

--- frontend/Our_Event_Platform/vite.config.js
+++ frontend/Our_Event_Platform/vite.config.js
@@ -1,15 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
+
+const env = loadEnv(
+  'all',
+  process.cwd()
+);
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,  // Specify the port
-    host: 'localhost',  // Bind to localhost
-    strictPort: true,  // Ensures Vite uses the specified port only
     proxy: {
       '/api': {
-        target: 'http://localhost:5000', // Backend API server
+        target: env.VITE_SERVER_TARGET,
         changeOrigin: true,
         secure: false
       }
